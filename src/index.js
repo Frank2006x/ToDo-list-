c32086764fd3a77aed0447c68910dcb67de88d1f
Frank2006x <frank2006x@gmail.com>
2025-02-17 22:30:53 +0530

    input-form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles.css';
 import setactive from './active.js'
-import openDialog from './inputform.js';
+import { openDialog,setClicked,closeDialog } from './inputform.js';
 window.setactive = setactive;
 window.openDialog=openDialog;
+window.setClicked=setClicked;
+window.closeDialog=closeDialog;
--- src/inputform.js
+++ src/inputform.js
@@ -1,5 +1,16 @@
-export default function openDialog(){
+export  function openDialog(){
     document.getElementById("input_form").showModal();
     console.log("hi");
 }
+export function closeDialog(){
+    document.getElementById("input_form").close();
+}
 // export
+
+export function setClicked(Element){
+    document.querySelectorAll(".pri-btn").forEach(btn=>{
+        btn.classList.remove("click");
+    })
+    Element.classList.add("click");
+}
+        

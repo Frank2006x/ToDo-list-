d09cbe983dec98db365341de791dfc8765c7413b
Frank2006x <frank2006x@gmail.com>
2025-02-19 22:43:24 +0530

    code-refactor bug fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
 import setactive from './active.js'
-import { openDialog,setClicked,closeDialog } from './inputform.js';
+import inputForm from './inputform.js';
 window.setactive = setactive;
-window.openDialog=openDialog;
-window.setClicked=setClicked;
-window.closeDialog=closeDialog;
+window.inputForm=inputForm;
+document.addEventListener("DOMContentLoaded", ()=> {
+    console.log("DOM is fully loaded!");
+    inputForm.enableClickOutside(); 
+});
--- src/inputform.js
+++ src/inputform.js
@@ -1,16 +1,43 @@
-export  function openDialog(){
-    document.getElementById("input_form").showModal();
-    console.log("hi");
-}
-export function closeDialog(){
-    document.getElementById("input_form").close();
-}
-// export
-
-export function setClicked(Element){
-    document.querySelectorAll(".pri-btn").forEach(btn=>{
-        btn.classList.remove("click");
-    })
-    Element.classList.add("click");
-}
+export default class inputForm{
+    static openDialog(){
+        document.getElementById("input_form").showModal();
+        console.log("hi");
+    }
+    static closeDialog(){
+        document.getElementById("input_form").close();
+    }
+    static setClicked(Element){
+        document.querySelectorAll(".pri-btn").forEach(btn=>{
+            btn.classList.remove("click");
+        })
+        Element.classList.add("click");
+        const value=document.getElementById("pri-value");
+        value.textContent=Element.innerHTML;
+        console.log(value.textContent);
+    }
+    static submitData(){
+        const name=document.getElementById("name").value;
+        const desc=document.getElementById("desc").value;
+        const pri=document.getElementById("pri-value").value;
+        // const date=document.getElementById("due_date").value;
+        let dataSet={"name":name,"desc":desc,"pri":pri};
+        console.log(dataSet);
+        inputForm.closeDialog();
+    }
+    static enableClickOutside() {
+        const dialog = document.getElementById("input_form");
+    
+        if (!dialog) {
+            console.error("Dialog not found!");
+            return;
+        }
         
+        
+        dialog.addEventListener("click", (e) => {
+            if (e.target === dialog) {
+                inputForm.closeDialog();
+            }
+        });
+    }
+
+}
